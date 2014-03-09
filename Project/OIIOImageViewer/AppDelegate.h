9fb204d763dda2658e730acf1744be429e8a7d1c
wilg <wil@wilgieseler.com>
2014-03-09 16:58:40 -0700

    added drag and drop for testing porpoises

--- Project/OIIOImageViewer/AppDelegate.h
+++ Project/OIIOImageViewer/AppDelegate.h
@@ -9,7 +9,7 @@
 #import <Cocoa/Cocoa.h>
 #import "NSImage+OIIO.h"
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate, NSWindowDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
 @property (assign) IBOutlet NSImageView *imageView;
--- Project/OIIOImageViewer/AppDelegate.m
+++ Project/OIIOImageViewer/AppDelegate.m
@@ -11,20 +11,51 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    
+    // Setup drag and drop.
+    [self.imageView unregisterDraggedTypes];
+    [self.imageView.window registerForDraggedTypes:@[NSFilenamesPboardType]];
+    [self.imageView.window setDelegate:self];
+    
 
     // Find data of image in bundle
-//    NSURL *file = [[NSBundle mainBundle] URLForResource:@"Digital_LAD_2048x1556" withExtension:@"dpx"];
-    NSURL *file = [[NSBundle mainBundle] URLForResource:@"test" withExtension:@"png"];
+    NSURL *file = [[NSBundle mainBundle] URLForResource:@"Digital_LAD_2048x1556" withExtension:@"dpx"];
 
-    // Initialize an image from URL.
-    NSImage *image = [NSImage oiio_imageWithContentsOfURL:file];
+    // Initialize an image from URL. Always use OpenImageIO.
+    NSImage *image = [NSImage oiio_forceImageWithContentsOfURL:file];
 
     // Display it
-    self.imageView.image = image;
+    [self setImage:image];
+}
 
+- (void)setImage:(NSImage *)image {
+    self.imageView.image = image;
+    
     NSLog(@"Image: %@", image);
     NSLog(@"Image Metadata: %@", image.ooio_metadata);
+}
 
+#pragma mark -
+#pragma mark Window Delegate
+    
+-(NSDragOperation)draggingEntered:(id < NSDraggingInfo >)sender {
+    return NSDragOperationGeneric;
+}
+
+    
+-(BOOL)prepareForDragOperation:(id < NSDraggingInfo >)sender {
+    return YES;
+}
+    
+- (BOOL)performDragOperation:(id < NSDraggingInfo >)sender {
+    NSPasteboard *pboard = [sender draggingPasteboard];
+    if ( [[pboard types] containsObject:NSFilenamesPboardType] ) {
+        [NSApp activateIgnoringOtherApps:YES];
+        NSArray *files = [pboard propertyListForType:NSFilenamesPboardType];
+        [self setImage:[NSImage oiio_forceImageWithContentsOfURL:[NSURL fileURLWithPath:files[0]]]];
+        return YES;
+    }
+    return NO;
 }
 
 @end
