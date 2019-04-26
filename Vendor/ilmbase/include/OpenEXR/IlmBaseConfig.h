4f4c3158fba5f96babdbee2f5404cac22433cf8f
Greg Cotten <greg@gregcotten.com>
2019-04-26 10:03:17 -0700

    2.0.7 oneshot
    compiled on 10.14

--- Vendor/ilmbase/include/OpenEXR/IlmBaseConfig.h
+++ Vendor/ilmbase/include/OpenEXR/IlmBaseConfig.h
@@ -1,5 +1,12 @@
 /* config/IlmBaseConfig.h.  Generated from IlmBaseConfig.h.in by configure.  */
 //
+// Define and set to 1 if the target system has c++11/14 support
+// and you want IlmBase to NOT use it's features
+//
+
+/* #undef ILMBASE_FORCE_CXX03 */
+
+//
 // Define and set to 1 if the target system has POSIX thread support
 // and you want IlmBase to use it for multithreaded file I/O.
 //
@@ -36,9 +43,9 @@
 // client namespaces.
 //
 #define ILMBASE_INTERNAL_NAMESPACE_CUSTOM 1
-#define IMATH_INTERNAL_NAMESPACE Imath_2_2
-#define IEX_INTERNAL_NAMESPACE Iex_2_2
-#define ILMTHREAD_INTERNAL_NAMESPACE IlmThread_2_2
+#define IMATH_INTERNAL_NAMESPACE Imath_2_3
+#define IEX_INTERNAL_NAMESPACE Iex_2_3
+#define ILMTHREAD_INTERNAL_NAMESPACE IlmThread_2_3
 
 /* #undef ILMBASE_NAMESPACE_CUSTOM */
 #define IMATH_NAMESPACE Imath
@@ -57,12 +64,12 @@
 //
 // Version information
 //
-#define ILMBASE_VERSION_STRING "2.2.1"
-#define ILMBASE_PACKAGE_STRING "IlmBase 2.2.1"
+#define ILMBASE_VERSION_STRING "2.3.0"
+#define ILMBASE_PACKAGE_STRING "IlmBase 2.3.0"
 
 #define ILMBASE_VERSION_MAJOR 2
-#define ILMBASE_VERSION_MINOR 2
-#define ILMBASE_VERSION_PATCH 1
+#define ILMBASE_VERSION_MINOR 3
+#define ILMBASE_VERSION_PATCH 0
 
 // Version as a single hex number, e.g. 0x01000300 == 1.0.3
 #define ILMBASE_VERSION_HEX ((ILMBASE_VERSION_MAJOR << 24) | \
